65537a878852b8718562c56684c8b9879a5e8def
Rosie Egle <regle@reibus.com>
2025-07-10 15:04:11 -0400

    Inital supbase + vite setup

---
+++ supabase-react/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { supabase } from "clients/supabaseClient";
+import Account from "components/Account";
+import Auth from "components/Auth";
+import "./App.css";
+
+const App = () => {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ supabase-react/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["crypto"],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+      components: path.resolve(__dirname, "src/components"),
+      clients: path.resolve(__dirname, "src/clients"),
+    },
+  },
+});
