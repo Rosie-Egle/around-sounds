dc4939db2db966c8d9be9b472bb0dba6f8f0ca00
Rosie Egle <regle@reibus.com>
2025-07-12 13:34:31 -0400

    main to ts file

--- supabase-react/src/main.tsx
+++ supabase-react/src/main.tsx
@@ -3,9 +3,12 @@ import "@fontsource/lato/400.css";
 import "@fontsource/lato/700.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
